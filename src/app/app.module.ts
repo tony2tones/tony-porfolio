a3a93a74d9587bda277a12855d32a97b1813c983
tony2tones <anthonymarques@outlook.com>
2021-10-24 23:01:07 +0200

    added style animations and routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,20 @@ import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/landing/home.component';
 import { TechstackComponent } from './pages/techstack/techstack.component';
+import { HeadnavComponent } from './components/headnav/headnav.component';
 
 
 const routes: Routes = [
-  { path: '/', component: HomeComponent },
-  { path: '/techstack', component: TechstackComponent }
+  { path: '', component: HomeComponent },
+  { path: 'techstack', component: TechstackComponent }
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    TechstackComponent
+    TechstackComponent,
+    HeadnavComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/headnav/headnav.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-headnav',
+  templateUrl: './headnav.component.html',
+  styleUrls: ['./headnav.component.css']
+})
+export class HeadnavComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
