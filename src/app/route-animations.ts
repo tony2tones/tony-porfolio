3f78d896a4afc31644b9b897bf2604b196dd72fc
Anthony <anthonymarques@outlook.com>
2023-02-11 10:22:02 +0200

    styles and animations are working properly finally

--- src/app/route-animations.ts
+++ src/app/route-animations.ts
@@ -1,32 +1,32 @@
-import {
-    trigger,
-    transition,
-    style,
-    query,
-    group,
-    animateChild,
-    animate,
-    keyframes,
-} from '@angular/animations';
+// import {
+//     trigger,
+//     transition,
+//     style,
+//     query,
+//     group,
+//     animateChild,
+//     animate,
+//     keyframes,
+// } from '@angular/animations';
 
-// Basic
+// // Basic
 
-export const fader = trigger('routeAnimations', [
-    transition('* <=> *', [
-        query(':enter, :leave', [
-            style({
-                position: 'absolute',
-                left: 0,
-                width: '100vw',
-                height: '100vh',
-                opacity: 0,
-                transform: 'scale(0) translateY(100%)',
-            }),
-        ]),
-        query(':enter', [
-            animate('600ms ease', 
-            style({opacity: 1, transform: 'scale(1) translateY(0)'})
-            ),
-        ])
-    ]),
-])
+// export const fader = trigger('routeAnimations', [
+//     transition('* <=> *', [
+//         query(':enter, :leave', [
+//             style({
+//                 position: 'absolute',
+//                 left: 0,
+//                 width: '100vw',
+//                 height: '100vh',
+//                 opacity: 0,
+//                 transform: 'scale(0) translateY(100%)',
+//             }),
+//         ]),
+//         query(':enter', [
+//             animate('600ms ease', 
+//             style({opacity: 1, transform: 'scale(1) translateY(0)'})
+//             ),
+//         ])
+//     ]),
+// ])
